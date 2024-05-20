5e6ee898b162e753ce83f9fbfce52a927af1e1d0
Mateusz Mackowiak <mateusz.mackowiak1@o2.pl>
2024-05-20 11:22:25 +0200

    Usprawnienie Keyboard inputs i Mouse inputs

--- src/inputs/Keyboard.java
+++ src/inputs/Keyboard.java
@@ -1,9 +1,16 @@
 package inputs;
 
+import main.GamePanel;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Keyboard implements KeyListener {
+    private GamePanel gamePanel;
+    public Keyboard(GamePanel gamePanel){
+        this.gamePanel = gamePanel;
+
+    }
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -14,16 +21,16 @@ public class Keyboard implements KeyListener {
 
         switch (e.getKeyCode()){
             case KeyEvent.VK_W:
-                System.out.println("W");
+                gamePanel.changeYDelta(-1);
                 break;
             case KeyEvent.VK_A:
-                System.out.println("A");
+                gamePanel.changeXDelta(-1);
                 break;
             case KeyEvent.VK_S:
-                System.out.println("S");
+                gamePanel.changeYDelta(1);
                 break;
             case KeyEvent.VK_D:
-                System.out.println("D");
+                gamePanel.changeXDelta(1);
                 break;
 
         }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,21 +1,43 @@
 package main;
 
 import inputs.Keyboard;
+import inputs.Mouse;
 
 import javax.swing.*;
 import java.awt.*;
 
 //Game Panel to jest to bo będzie widoczne w srodku okna "Window"
 public class GamePanel extends JPanel {
+    private Mouse mouse;
+    private int xDelta = 30, yDelta = 30;
+
     public GamePanel(){
 
-        addKeyListener(new Keyboard());
-
+        mouse = new Mouse(this);
+        addKeyListener(new Keyboard(this));
+        addMouseListener(mouse);
+        addMouseMotionListener(mouse);
     }
+
+    public void changeXDelta(int value){
+        this.xDelta+=value;
+        repaint();
+    }
+    public void changeYDelta(int value){
+        this.yDelta+=value; //zmiana pozycji o okresloną w Keyboard wartość.
+        repaint(); //odświeżanie po zmianie pozycji.
+    }
+//porusza się z kursorem.
+    public void setByMouse(int x, int y){
+        this.xDelta = x;
+        this.yDelta = y;
+        repaint();
+    }
+
     //paintComponent do "malowania" w oknie, czyszczenia i tworzenia grafik na nowo.
     public void paintComponent(Graphics g){
         super.paintComponent((g));
 
-        g.fillRect(30,30,300,300);
+        g.fillRect(xDelta, yDelta,300,300);
     }
 }
