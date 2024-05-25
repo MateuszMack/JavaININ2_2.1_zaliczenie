2c47e5c8707df3447a14e738039a7a48e6ab96d9
Norbert Bilinski <norbertbilinski1@gmail.com>
2024-05-25 17:50:43 +0200

    pouczenie2 kamila2136
    Signed-off-by: Norbert Bilinski <norbertbilinski1@gmail.com>

--- src/main/Game.java
+++ src/main/Game.java
@@ -10,6 +10,26 @@ public class Game {
         gamePanel.requestFocus();
 
     }
+
+    public void update() { switch (Gamestate.state) { case MENU:
+        break;
+        case PLAYING:
+            levelManager.update();
+            player.update();
+            break;
+        default:
+            break;
+    }
+    }
+    public void render (Graphics g) {
+        switch (Gamestate.state) { case MENU:
+        break; case PLAYING:
+        levelManager.draw(g);
+        player.render(g);
+        break;
+        default: break;
+    }
+}
 }
 //kupcia dupcia
 //Kamil to nie jest to co my mamy robić!
