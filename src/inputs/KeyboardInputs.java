9acd82e0c5da88cf09a1de4f359bfb4aae08c83c
Mateusz Mackowiak <mateusz.mackowiak1@o2.pl>
2024-06-01 21:01:24 +0200

    aktualizacja poruszania plus zwiększenie płynności. nowe klasy

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -3,10 +3,8 @@ package inputs;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-import main.Game;
 import main.GamePanel;
-import static utilz.Constans.Directions.*;
-
+import static utilz.Constants.Directions.*;
 
 public class KeyboardInputs implements KeyListener {
 
@@ -18,40 +16,41 @@ public class KeyboardInputs implements KeyListener {
 
     @Override
     public void keyTyped(KeyEvent e) {
-
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
+                gamePanel.getGame().getPlayer().setUp(false);
+                break;
             case KeyEvent.VK_A:
+                gamePanel.getGame().getPlayer().setLeft(false);
+                break;
             case KeyEvent.VK_S:
+                gamePanel.getGame().getPlayer().setDown(false);
+                break;
             case KeyEvent.VK_D:
-                gamePanel.setMoving(false);
+                gamePanel.getGame().getPlayer().setRight(false);
                 break;
         }
-
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
-                gamePanel.setDirection(UP);
+                gamePanel.getGame().getPlayer().setUp(true);
                 break;
             case KeyEvent.VK_A:
-                gamePanel.setDirection(LEFT);
+                gamePanel.getGame().getPlayer().setLeft(true);
                 break;
             case KeyEvent.VK_S:
-                gamePanel.setDirection(DOWN);
+                gamePanel.getGame().getPlayer().setDown(true);
                 break;
             case KeyEvent.VK_D:
-                gamePanel.setDirection(RIGHT);
+                gamePanel.getGame().getPlayer().setRight(true);
                 break;
         }
-
     }
-
 }
--- src/utilz/Constants.java
+++ src/utilz/Constants.java
@@ -1,15 +1,14 @@
 package utilz;
 
-public class Constans {
-
-    public static class Directions{
+public class Constants {
+    public static class Directions {
         public static final int LEFT = 0;
         public static final int UP = 1;
         public static final int RIGHT = 2;
         public static final int DOWN = 3;
     }
 
-    public static class PlayerConstans{
+    public static class PlayerConstants {
         public static final int IDLE = 0;
         public static final int RUNNING = 1;
         public static final int JUMP = 2;
@@ -20,8 +19,8 @@ public class Constans {
         public static final int ATTACK_JUMP_1 = 7;
         public static final int ATTACK_JUMP_2 = 8;
 
-        public static int GetSpriteAmount(int player_action){
-            switch (player_action){
+        public static int GetSpriteAmount(int player_action) {
+            switch (player_action) {
                 case RUNNING:
                     return 6;
                 case IDLE:
@@ -41,4 +40,5 @@ public class Constans {
             }
         }
     }
+
 }
