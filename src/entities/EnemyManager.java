12bba450f6bc6299f080dcc969c5de938256426f
Dariusz81061 <gax36155@student.gdynia.merito.pl>
2024-06-14 17:13:08 +0200

    Epizod 16 part: 4

--- src/entities/EnemyManager.java
+++ src/entities/EnemyManager.java
@@ -1,10 +1,13 @@
 package entities;
 
-import utilz.Constants;
-
-import java.awt.*;
 import java.awt.Graphics;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import gamestates.Playing;
+import utilz.LoadSave;
+import static utilz.Constants.EnemyConstants.*;
 
 public class EnemyManager {
 
@@ -23,8 +26,9 @@ public class EnemyManager {
     }
 
     public void update(int[][] lvlData, Player player) {
-        for(Crabby c : crabbies)
-            c.update(lvlData, player);
+        for (Crabby c : crabbies)
+            if (c.isActive())
+                c.update(lvlData, player);
     }
 
     public void draw(Graphics g, int xLvlOffset) {
@@ -32,8 +36,24 @@ public class EnemyManager {
     }
 
     private void drawCrabs(Graphics g, int xLvlOffset) {
-        for(Crabby c : crabbies)
-            g.drawImage(crabbyArr[c.getEnemyState()][c.getAniIndex()], (int)c.getHitbox().x - xLvlOffset, (int) c.getHitbox().y, CRABBY, CRABBY_WIDTH, CRABBY_HEIGHT, null);
+        for (Crabby c : crabbies)
+            if (c.isActive()) {
+                g.drawImage(crabbyArr[c.getEnemyState()][c.getAniIndex()], (int) c.getHitbox().x - xLvlOffset - CRABBY_DRAWOFFSET_X + c.flipX(), (int) c.getHitbox().y - CRABBY_DRAWOFFSET_Y,
+                        CRABBY_WIDTH * c.flipW(), CRABBY_HEIGHT, null);
+//				c.drawHitbox(g, xLvlOffset);
+//				c.drawAttackBox(g, xLvlOffset);
+
+            }
+
+    }
+
+    public void checkEnemyHit(Rectangle2D.Float attackBox) {
+        for (Crabby c : crabbies)
+            if (c.isActive())
+                if (attackBox.intersects(c.getHitbox())) {
+                    c.hurt(10);
+                    return;
+                }
     }
 
     private void loadEnemyImgs() {
@@ -42,5 +62,10 @@ public class EnemyManager {
         for(int j = 0; j < crabbyArr.length; j++)
             for(int i = 0; i < crabbyArr[j].length; i++)
                 crabbyArr[j][i] = temp.getSubimage(i * CRABBY_WIDTH_DEFAULT, j * CRABBY_HEIGHT_DEFAULT, CRABBY_WIDTH_DEFAULT, CRABBY_HEIGHT_DEFAULT);
+    }
+
+    public void resetAllEnemies() {
+        for (Crabby c : crabbies)
+            c.resetEnemy();
     }
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,53 +1,43 @@
 package main;
 
-import inputs.Keyboard;
-import inputs.Mouse;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+import static main.Game.GAME_HEIGHT;
+import static main.Game.GAME_WIDTH;
 
-import javax.swing.*;
-import java.awt.*;
-
-//Game Panel to jest to bo będzie widoczne w srodku okna "Window"
 public class GamePanel extends JPanel {
-    private Mouse mouse;
-    private int xDelta = 30, yDelta = 30;
-    private int frames=0;
-    private long lastCheck = 0;
 
-    public GamePanel(){
+    private MouseInputs mouseInputs;
+    private Game game;
 
-        mouse = new Mouse(this);
-        addKeyListener(new Keyboard(this));
-        addMouseListener(mouse);
-        addMouseMotionListener(mouse);
+    public GamePanel(Game game) {
+        mouseInputs = new MouseInputs(this);
+        this.game = game;
+        setPanelSize();
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
     }
 
-    public void changeXDelta(int value){
-        this.xDelta+=value;
-    }
-    public void changeYDelta(int value){
-        this.yDelta+=value; //zmiana pozycji o okresloną w Keyboard wartość.
-    }
-//porusza się z kursorem.
-    public void setByMouse(int x, int y){
-        this.xDelta = x;
-        this.yDelta = y;
+    private void setPanelSize() {
+        Dimension size = new Dimension(GAME_WIDTH, GAME_HEIGHT);
+        setPreferredSize(size);
     }
 
-    //paintComponent do "malowania" w oknie, czyszczenia i tworzenia grafik na nowo.
-    public void paintComponent(Graphics g){
-        super.paintComponent((g));
-
-        g.fillRect(xDelta, yDelta,300,300);
-
-        frames++;
-        if(System.currentTimeMillis() - lastCheck >=1000){
-            lastCheck = System.currentTimeMillis();
-            System.out.println("FPS: "+ frames);
-            frames = 0;
-
-        }
-
-        repaint(); //odświeżanie po zmianie pozycji.
+    public void updateGame() {
 
     }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        game.render(g);
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
 }
